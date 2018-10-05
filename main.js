e721a6d633f92431e5f73fdaaa6729838e2d8f91
farid landi <faridvatani@gmail.com>
2018-10-06 00:49:10 +0330

    initial electron.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const {app , BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+app.on('ready' , function(){
+   let mainWin = new BrowserWindow({width : 800 , height : 600});
+   mainWin.loadURL(url.format({
+    pathname : path.join(__dirname , "index.html"),
+    protocol : 'file:'
+    }));
+})
