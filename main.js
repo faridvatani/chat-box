40cd3582a3902135b4400fe180aa31d43fe153a3
farid landi <faridvatani@gmail.com>
2018-10-07 21:44:29 +0330

    install electron-reload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import {app , BrowserWindow} from 'electron';
-const url = require('url');
-const path = require('path');
+import url from 'url';
+import path from 'path';
+import ElectronReload from 'electron-reload';
+ElectronReload(__dirname);
 
 app.on('ready' , function(){
    let mainWin = new BrowserWindow({width : 800 , height : 600});
