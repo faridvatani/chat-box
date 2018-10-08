a18811f412861b679b834535b9dee9c1d8f27d02
farid landi <faridvatani@gmail.com>
2018-10-08 23:26:49 +0330

    install devtronn

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import url from 'url';
 import path from 'path';
 import ElectronReload from 'electron-reload';
 ElectronReload(__dirname);
+import devtron from 'devtron';
 
 app.on('ready' , function(){
+   devtron.install();
    let mainWin = new BrowserWindow({width : 800 , height : 600});
    mainWin.loadURL(url.format({
     pathname : path.join(__dirname , "index.html"),
