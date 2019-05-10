92a70a547636814d87f2cb3f77b6d2fec278c61c
farid landi <faridvatani@gmail.com>
2019-05-10 20:18:08 +0430

    change title and url to chatbox

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.on('ready' , () => {
     splashScreen.on('closed' , () => splashScreen = null );
 
 
-    mainWin.loadURL('http://github.com');
+    mainWin.loadURL('http://aparat.com/');
     splashScreen.loadURL(`file://${__dirname}/index.html`);
 
     mainWin.on('ready-to-show' , () => {
